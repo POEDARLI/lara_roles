15ef8650bb152432bef3d8b1c45788a2bf277e9b
poedarli <poepoe.dev.1991@gmail.com>
2021-06-08 18:09:21 +0800

    add Globalscope at Article, show Articles related to Login User

--- app/Article.php
+++ app/Article.php
@@ -3,10 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Article extends Model
 {
     protected $fillable = [
         'title', 'full_text', 'category_id', 'user_id'
     ];
+
+    protected static function booted()
+    {
+        if (auth()->check()) {
+            static::addGlobalScope('user', function (Builder $builder) {
+                $builder->where('user_id', auth()->id());
+            });
+        }
+    }
 }
